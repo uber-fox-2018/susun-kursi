fae2c191839b7e64a963d658abcd794dc88fe681
joanlamrack <joanlamrack@gmail.com>
2018-07-10 11:16:32 +0700

    Added code implementations

--- susunKursi.js
+++ susunKursi.js
@@ -8,35 +8,34 @@
 // Baris 1 :  [ 'b', 'e', 'h' ]
 // Baris 2 :  [ 'c', 'f', 'i' ]
 
-'use strict'
+"use strict";
 
 function generateSeats(row) {
-	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	let seats = [];
+	for(let i=0; i<row; i++) {
+		seats.push([]);
 	}
-	return seats
+	return seats;
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
-	let row = 0
-
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
-			row++
+	let seats = generateSeats(rowSeats);
+	let row = 0;
+	for(let i=0; i<arr.length; i++) {
+		seats[row].push(arr[i]);
+		if(row < 2) {
+			row++;
 		} else {
-			row == 0
+			row = 0;
 		}
 	}
-	printSeats(seats)
+	printSeats(seats);
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
-		console.log(`Baris ${i} : `, seats[i])
+	for(let i=0; i<seats.length; i++) {
+		console.log(`Baris ${i} : `, seats[i]);
 	}
 }
 
-managePerson(['a','b','c','d','e','f','g','h','i','j'], 3)
+managePerson(["a","b","c","d","e","f","g","h","i","j"], 3);
