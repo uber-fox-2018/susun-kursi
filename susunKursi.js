644cfee4a90dd8bd5ae38e9b4bbc155d580d5cca
Wahyudi Setiaji <wahyudisetiaji@gmail.com>
2018-07-10 11:18:04 +0700

    Susun Kursi Done

--- susunKursi.js
+++ susunKursi.js
@@ -12,29 +12,29 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<row; i++) { // i<rowa ===> penulisan typo
+		seats.push([]) // seat.push([]) ===> penulisan seat kurang s
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats) // generateSeat(rowSeats) ===> nama function kurang s
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i=0; i<= arr.length-1; i++) { // i<= arr.length-1 ===> kurang sama dengan dalam kondisi
+		seats[row].push(arr[i]) // seat.row[] ===> penulisan seat kurang s
+		if(row < 2) { // if(row <= 2) ===> diganti tidak pakai sama dengan
 			row++
 		} else {
-			row == 0
+			row = 0 // row == 0 ===> kelebihan sama dengan
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<seats.length; i++) { // let i=1 ===> dimulai dari 0
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
